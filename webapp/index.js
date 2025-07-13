6aa845d99a73124d3d4f9702b43b39e616972fbe
An Ngo <annc.21198@gmail.com>
2025-07-13 17:02:01 +0700

    feat: initialize UI5 project with basic configuration and files
    Add initial project setup including .prettierignore, .gitignore, .prettierrc, ui5.yaml, package.json, and webapp files.
    Configure UI5 framework version and libraries, add basic index.html and index.js.
    Set up package.json with UI5 CLI and start script.

---
+++ webapp/index.js
@@ -0,0 +1,12 @@
+// prettier-ignore-file
+sap.ui.define([], () => {
+	'use strict';
+
+	alert('a');
+	// new Button({
+	// 	text: 'Ready...',
+	// 	press() {
+	// 		MessageToast.show('Hello World!');
+	// 	},
+	// }).placeAt('content');
+});
